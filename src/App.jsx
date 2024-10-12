73a0eb9211f4d93f7ee7618ff584369640f728cd
brahamzapata1992 <brahamzapata1992@gmail.com>
2024-10-12 16:04:46 -0400

    solucioin error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Projects from  './pages/Projects'
 import Contact from './pages/Contact'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
