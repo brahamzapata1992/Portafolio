7bd226ecfa9744ee70b72474185e07d1df978ee7
brahamzapata1992 <brahamzapata1992@gmail.com>
2024-10-13 00:26:04 -0400

    creando home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaHome, FaUser, FaFolder, FaEnvelope } from 'react-icons/fa'; // Importa los íconos que necesitas
+import { Link, useLocation } from 'react-router-dom';
+import { FaHome, FaUser, FaFolder, FaEnvelope } from 'react-icons/fa';
 import './Footer.css';
 
 const Footer = () => {
+  const location = useLocation();
+
   return (
     <footer className="footer">
       <div className="nav-icons">
-        <Link to="/"><FaHome /></Link>  
-        <Link to="/about"><FaUser /></Link>  
-        <Link to="/projects"><FaFolder /></Link>  
-        <Link to="/contact"><FaEnvelope /></Link>  
+        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+          <FaHome />
+        </Link>
+        <Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>
+          <FaUser />
+        </Link>
+        <Link to="/projects" className={location.pathname === '/projects' ? 'active' : ''}>
+          <FaFolder />
+        </Link>
+        <Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>
+          <FaEnvelope />
+        </Link>
       </div>
     </footer>
   );
 };
 
-
 export default Footer;
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from 'react';
+import perfil from '../assets/perfil.JPG'
+import { useTheme } from '../context/ThemeContext';
+import './Home.css'; 
 
 const Home = () => {
+
+  const { darkMode } = useTheme(); 
+
   return (
-    <div>
-      Home
+    <div className="home">
+      <div className='contener_foto_perfil'>
+        <img src={perfil} alt="Tu Nombre" className="profile-photo" />
+      </div>
+      <div>
+        <h1 className='home_nombre'>I'M BRAHAM ZAPATA</h1>
+        <h2 className='home_profesion'>WEB DESIGNER</h2>
+        <p className='home_texto_presentacion'>I am a developer passionate about creating intuitive web applications that enhance the user experience. I focus on learning and growing professionally, collaborating on projects that make life easier and more productive.</p>
+        <button className="contact-button">Contactar</button>
+      </div>     
     </div>
-  )
-}
+  );
+};
 
-
-export default Home
+export default Home;
