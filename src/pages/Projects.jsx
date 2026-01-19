f28a2d4e6d1f9c5869ef37593d83c15f4c428718
brahamzapata1992 <brahamzapata1992@gmail.com>
2026-01-18 19:51:21 -0500

    Actualizando Seccion del Portafolio

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './Projects.css'
 
 const Proyects = () => {
   return (
-    <div>
-      Projects
+    <div className='principal_contenedor_all'>
+      <div className='contenedor_projects_principal_text'>
+        <h1 className='about_text_title'>MY <span className='projects_letra_me'>PORTFOLIO</span></h1>        
+      </div>
     </div>
   )
 }
